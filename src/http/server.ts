638d449b65bc77e7c56ef96b63a7508412efcaac
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2024-09-29 22:56:29 -0300

    feat: Add endpoint for creating goals

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,30 @@
 import fastify from 'fastify';
- 
-const server = fastify();
+import { createGoal } from '../features/create-goal';
+import z from 'zod';
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
+
+const server = fastify().withTypeProvider<ZodTypeProvider>();
+
+// Add schema validator and serializer
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
+
+server.post('/goals', {
+  schema: {
+    body: z.object({
+      title: z.string(),
+      weeklyFrequency: z.number().int().min(1).max(7),
+    }),
+  }
+},
+async (request) => {
+  const { title, weeklyFrequency } = request.body;
+  await createGoal({
+    title,
+    weeklyFrequency,
+  });
+});
+
 
 server.listen({
   port: 3333,
