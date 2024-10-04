44c073b154c53d7b4f8c2aae6f3b90118ac835df
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2024-10-04 19:43:44 -0300

    feat: Add endpoint for getting pending goals and creating goal completions

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,12 +2,20 @@ import fastify from 'fastify';
 import { createGoal } from '../features/create-goal';
 import z from 'zod';
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
+import { getWeekPendingGoals } from '../features/get-week-pending-goals';
+import { createGoalsCompletions } from '../features/create-goal-completion';
 
 const server = fastify().withTypeProvider<ZodTypeProvider>();
 
 // Add schema validator and serializer
 server.setValidatorCompiler(validatorCompiler);
 server.setSerializerCompiler(serializerCompiler);
+
+server.get('/pending-goals', async () => {
+  const sql = getWeekPendingGoals();
+
+  return sql;  
+});
 
 server.post('/goals', {
   schema: {
@@ -25,6 +33,20 @@ async (request) => {
   });
 });
 
+server.post('/completions', {
+  schema: {
+    body: z.object({
+      goalId: z.string(),
+    }),
+  },
+},
+async (request) => {
+  const { goalId } = request.body;
+  await createGoalsCompletions({
+    goalId,
+  });
+},
+);
 
 server.listen({
   port: 3333,
