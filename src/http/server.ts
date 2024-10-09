997b64c06c4485e564a3f61fad697fb1177a7726
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2024-10-08 22:23:25 -0300

    refactor: Update server routes for manipulating goals

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,58 +1,20 @@
 import fastify from 'fastify';
-import { createGoal } from '../features/create-goal';
-import z from 'zod';
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
-import { getWeekPendingGoals } from '../features/get-week-pending-goals';
-import { createGoalsCompletions } from '../features/create-goal-completion';
-import { getWeekSummary } from '../features/get-week-summary';
+import { getWeekPendingGoalsRoute } from './routes/get-week-pending-goals';
+import { createGoalCompletionRoute } from './routes/create-goal-completion';
+import { createGoalRoute } from './routes/create-goal';
+import { getWeekSummaryRoute } from './routes/get-week-summary';
 
-const server = fastify().withTypeProvider<ZodTypeProvider>();
+export const server = fastify().withTypeProvider<ZodTypeProvider>();
 
 // Add schema validator and serializer
 server.setValidatorCompiler(validatorCompiler);
 server.setSerializerCompiler(serializerCompiler);
 
-server.get('/pending-goals', async () => {
-  const sql = getWeekPendingGoals();
-
-  return sql;  
-});
-
-server.get('/summary', async() => {
-  const summary = await getWeekSummary();
-  return summary;
-});
-
-server.post('/goals', {
-  schema: {
-    body: z.object({
-      title: z.string(),
-      weeklyFrequency: z.number().int().min(1).max(7),
-    }),
-  }
-},
-async (request) => {
-  const { title, weeklyFrequency } = request.body;
-  await createGoal({
-    title,
-    weeklyFrequency,
-  });
-});
-
-server.post('/completions', {
-  schema: {
-    body: z.object({
-      goalId: z.string(),
-    }),
-  },
-},
-async (request) => {
-  const { goalId } = request.body;
-  await createGoalsCompletions({
-    goalId,
-  });
-},
-);
+server.register(getWeekPendingGoalsRoute);
+server.register(getWeekSummaryRoute);
+server.register(createGoalRoute);
+server.register(createGoalCompletionRoute);
 
 server.listen({
   port: 3333,
